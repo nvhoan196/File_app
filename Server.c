ea55521ee61dcd1b7415897f218cb5014a334b33
nvhoan196 <nvhoan196@gmail.com>
2018-12-13 12:07:23 +0700

    no message

--- Server.c
+++ Server.c
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <dirent.h>
 
 #define BUFF_SIZE 1024
 
---
+++ folder.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+
+
+int main() {
+	DIR *dir;
+	struct dirent *ent;
+	if ((dir = opendir("./test_folder")) != NULL) {
+  /* print all the files and directories within directory */
+		while ((ent = readdir (dir)) != NULL) {
+			printf ("%s\n", ent->d_name);
+		}
+		closedir (dir);
+	} else {
+  /* could not open directory */
+		perror ("");
+		return 1;
+	}
+}
